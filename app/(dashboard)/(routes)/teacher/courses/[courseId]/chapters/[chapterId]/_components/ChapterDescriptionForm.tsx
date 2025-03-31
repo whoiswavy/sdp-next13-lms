b10dc5de8333115c6add60be6dbf51b9b3d8e9b1
0xSam <smorahschool@gmail.com>
2025-03-31 16:40:17 +0100

    typo in chapter description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterDescriptionForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterDescriptionForm.tsx
@@ -70,7 +70,7 @@ const ChapterDescriptionForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between ">
-        Course Description
+        Chapter Description
         <Button variant="ghost" onClick={toggleEdit}>
           {isEditing ? (
             <>
